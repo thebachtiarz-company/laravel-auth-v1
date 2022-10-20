db2dee6ad7a8f311f69c239348e78d6cceabaa1a
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2022-10-20 12:11:01 +0700

    update - pat repository

--- src/Repositories/PersonalAccessTokenRepository.php
+++ src/Repositories/PersonalAccessTokenRepository.php
@@ -81,7 +81,7 @@ class PersonalAccessTokenRepository
          */
         $_user = $this->user->getClassModel()::find(Auth::user()->id);
 
-        $_create = $_user->createToken(name: Str::uuid(), expiresAt: $this->user->getTokenExpiresAt());
+        $_create = $_user->createToken(name: Str::uuid()->toString(), expiresAt: $this->user->getTokenExpiresAt());
 
         return $_create;
     }
